e5872a81033ffedbebe6d984a519e3aee9133e21
Casey Foster <casey@caseywebdev.com>
2012-02-02 00:11:34 -0800

    cleaning up

--- website.rb
+++ website.rb
@@ -1,14 +1,5 @@
 require "sinatra"
 
-get "/test/:zing" do
-	@blah = "BLAH"
-	@sum = 5
-	erb :test
-end
-
-get "/sum/*" do
-	params[:splat][0].split("/").map{ |s| s.to_i }.inject(:+).to_s
-	"test
-	test
-	test"
+get "/" do
+	erb :index
 end
