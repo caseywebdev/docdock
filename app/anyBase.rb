a2f4ed455bb3c7f24f996c57a77ad259f785ba0b
Casey Foster <casey@caseywebdev.com>
2012-02-12 16:28:19 -0800

    It's working!
    There isn't much of a style yet, but the basic implementation is
    working.

---
+++ app/anyBase.rb
@@ -0,0 +1,36 @@
+module AnyBaseIToS
+	def b(base)
+		i = self
+		val = ""
+		len = base.length
+		pow = 0
+		begin
+			mod = i%len**(pow+1)
+			val += base[mod/len**pow]
+			i -= mod
+			pow += 1
+		end while i > 0
+		val.reverse
+	end
+end
+
+module AnyBaseSToI
+	def b(base)
+		val = 0
+		len = base.length
+		self.split("").reverse.each_with_index { |c, i| val += base.index(c)*len**i }
+		val
+	end
+end
+
+class Fixnum
+	include AnyBaseIToS
+end
+
+class Bignum
+	include AnyBaseIToS
+end
+
+class String
+	include AnyBaseSToI
+end
---
+++ app/main.rb
@@ -0,0 +1,56 @@
+require "sinatra"
+require "redis"
+require "json"
+require_relative "anyBase"
+
+configure do
+	#set the base string we'll use for converting from base 10
+	BASE_62 = ("0".."9").to_a.join("")+("a".."z").to_a.join("")+("A".."Z").to_a.join("")
+	#define our Redis instance
+	REDIS = Redis.new
+	REDIS.select 0
+	REDIS.setnx "_id", 0
+	#set the default page title, that is, if @title is never set
+	DEFAULT_TITLE = "docdock | The Dock for Docs"
+	#set the title appendage so that the final title looks like @title+TITLE_APPENDAGE
+	TITLE_APPENDAGE = " | docdock"
+	#number of recent docs to show
+	RECENT_DOCS = 10
+end
+
+get "/*" do
+	if params[:splat][0] != ""
+		@id = params[:splat][0]
+		if REDIS.exists @id
+			@doc = REDIS.get @id
+		else
+			redirect "/"
+		end
+	end
+	lastId = REDIS.get("_id").to_i
+	@recentDocs = []
+	RECENT_DOCS.times do |i|
+		id = lastId-i
+		if id > 0
+			idBase62 = id.b BASE_62
+			@recentDocs << { id: idBase62, doc: REDIS.get(idBase62) }
+		else
+			break
+		end
+	end
+	erb :index
+end
+
+post "*" do
+	response = {status: "missing param"}
+	if params["doc"]
+		if params["doc"].strip == ""
+			response["status"] = "doc empty"
+		else
+			id = REDIS.incr("_id").b BASE_62
+			REDIS.set id, params["doc"]
+			response["status"] = id
+		end
+	end
+	JSON.generate response
+end
